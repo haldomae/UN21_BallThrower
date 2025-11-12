9b1865b889db9d2fe4d80cd7b31de3fe1c17dc37
haldomae <hal.domae@gmail.com>
2025-11-12 14:56:45 +0900

    Rigidbodyのコンポーネントを取得

--- BallThrower.cs
+++ BallThrower.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class BallThrower : MonoBehaviour
 {
+    // 投げる力
+    float throwPower = 10f;
+    // Rigidbodyのコンポーネントを格納する変数
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // コンポーネントを取得
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
