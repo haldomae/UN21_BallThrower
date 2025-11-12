881ba30e230971c8bffadbe9aad451e77caeda6a
haldomae <hal.domae@gmail.com>
2025-11-12 16:11:17 +0900

    Prefabからボールを生成

--- BallThrower.cs
+++ BallThrower.cs
@@ -6,17 +6,13 @@ public class BallThrower : MonoBehaviour
 {
     // 投げる力
     public float throwPower = 10f;
-    // Rigidbodyのコンポーネントを格納する変数
-    private Rigidbody rb;
-
-    // 投げる対象のSphere(Ball)を格納する変数
-    public GameObject targetBall;
+    // Prefabを格納する変数
+    public GameObject ballPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        // コンポーネントを取得
-        rb = targetBall.GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
@@ -25,11 +21,25 @@ public class BallThrower : MonoBehaviour
         // スペースキーが押されたら
         if(Input.GetKeyDown(KeyCode.Space))
         {
+            // ボールの位置を決める(3D座標)
+            // GameManagerの位置
+            Vector3 ballPosition = transform.position;
+
+            // ボール複製
+            GameObject ball = Instantiate(
+                // 1. Prefab(元となるもの)
+                ballPrefab,
+                // 2. 生成位置(3D座標)
+                ballPosition,
+                // 3. 生成時の回転
+                Quaternion.identity
+            );
+
             // 前方向に力を加える
             // transform.forward このオブジェクトの前方向
             // throwPower 投げる力
             // ForceMode.Impulse 瞬間的に力を加える
-            rb.AddForce(
+            .AddForce(
                 transform.forward * throwPower,
                 ForceMode.Impulse
             );
